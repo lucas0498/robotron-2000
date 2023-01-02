3449274355f59a1b165c334e05aee2f1679241b6
Lucas Gonçalves <lucass.goncalves@outlook.com>
2023-01-02 12:56:40 -0300

    Atualizando o arquivo main js

--- Js/main.js
+++ Js/main.js
@@ -1,6 +1,11 @@
 const robotron = document.querySelector("#robotron")
-robotron.addEventListener('click', dizOi);
+robotron.addEventListener('click', () => {
+    console.log('Cliquei no Robo.')
+});
 
-function dizOi(){
-    console.log('oi');
+function dizOi(nome){
+    console.log(`Oi ${nome}`);
+    console.log('Bem-vindo ao Robotron 2000');
 }
+
+dizOi('Lucas');
